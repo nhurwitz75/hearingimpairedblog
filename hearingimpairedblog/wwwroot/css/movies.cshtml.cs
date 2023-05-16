d38fa0e4a8b33cade675047a4e7de3072ac372aa
rebecca.axelrod <rebecca.axelrod@WINAPH4QW3PVOHV>
2023-05-16 19:37:29 -0400

    Add project files.

---
+++ hearingimpairedblog/wwwroot/css/movies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hearingimpairedblog.wwwroot.css
+{
+    public class moviesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
